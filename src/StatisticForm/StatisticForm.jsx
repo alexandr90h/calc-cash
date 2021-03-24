18b1d3ebcf5496f5f5efe811e21ba78ad0197c78
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2021-03-24 20:28:15 +0200

    24.03.2021-2

--- src/StatisticForm/StatisticForm.jsx
+++ src/StatisticForm/StatisticForm.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import dateFormat from 'dateformat';
+import { Bar } from 'react-chartjs-2';
 
 dateFormat.i18n = {
   dayNames: [
@@ -34,9 +35,45 @@ export default function StatisticForm() {
     });
     return summ / i;
   };
+  const data = {
+    labels: ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'Пятниця', 'Субота'],
+    datasets: [
+      {
+        label: 'Середня сума',
+        // barPercentage: 0.7,
+        // categoryPercentage: 0.8,
+        // barThickness: 'flex',
+        data: [
+          Math.round(setAverageForDay(rows, 'Понеділок')),
+          Math.round(setAverageForDay(rows, 'Вівторок')),
+          Math.round(setAverageForDay(rows, 'Середа')),
+          Math.round(setAverageForDay(rows, 'Четвер')),
+          Math.round(setAverageForDay(rows, 'Пятниця')),
+          Math.round(setAverageForDay(rows, 'Субота')),
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
-    <div>
-      <ul>
+    <div className="statistic-box">
+      <ul className="statistic-list">
         <li>
           Понеділок:{' '}
           <span>{String(Math.round(setAverageForDay(rows, 'Понеділок')))}</span>
@@ -62,6 +99,20 @@ export default function StatisticForm() {
           <span>{String(Math.round(setAverageForDay(rows, 'Субота')))}</span>
         </li>
       </ul>
+      <Bar
+        data={data}
+        width={500}
+        // height={100}
+        options={{
+          scales: {
+            yAxes: [
+              {
+                stacked: 2000,
+              },
+            ],
+          },
+        }}
+      />
     </div>
   );
 }
